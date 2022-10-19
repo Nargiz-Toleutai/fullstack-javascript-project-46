78e2a92fae7a9b08d95791e53ec2a9dc2827b8ad
Nargiz-Toleutai <nargizabdullinova@gmail.com>
2022-10-19 20:21:27 +0600

    add 'option' and 'argument' commander methods calls to gendiff add .gitignore remove node_modules and all content

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,5 +4,9 @@ import { program } from 'commander';
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1> <filepath2>')
 
-program.parse(process.argv);
+program.parse();
+//const options = program.opts();
+//process.argv
