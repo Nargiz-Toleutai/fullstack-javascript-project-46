86cced194b7a587420142ce2438814a398419f64
Nargiz-Toleutai <nargizabdullinova@gmail.com>
2022-10-20 20:44:44 +0600

    add index.js, and call action in gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,37 @@
 #!/usr/bin/env node
 import { program } from 'commander';
+import genDiff from '../src/index.js';
+import process from "process";
+import fs from "fs";
+import path from "path";
+
+const currentWorkingDir = process.cwd();
+
+const prettyPrint = (result) => {
+    const content = Object.entries(result)
+        .map(([key, value]) =>`  ${key}: ${value}`)
+        .join('\n');
+    return `{\n${content}\n}`;
+};
 
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format')
-  .argument('<filepath1> <filepath2>')
+  .arguments('<filepath1> <filepath2>')
+  .action((path1, path2) => {
+    const filepath1 = path.resolve(currentWorkingDir, path1);
+    const filepath2 = path.resolve(currentWorkingDir, path2);
+
+    const file1 = fs.readFileSync(filepath1);
+    const file2 = fs.readFileSync(filepath2);
+
+    const myObj1 = JSON.parse(file1);
+    const myObj2 = JSON.parse(file2);
+
+    const result = genDiff(myObj1, myObj2);
+    console.log(prettyPrint(result));
+  })
 
 program.parse();
-//const options = program.opts();
-//process.argv
+
