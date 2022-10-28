dce10d9e19f325ec695befb41cf3dd8aaf3bed62
Nargiz-Toleutai <nargizabdullinova@gmail.com>
2022-10-28 17:32:10 +0600

    create index.js in formatter

--- src/index.js
+++ src/index.js
@@ -2,27 +2,13 @@ import parse from '../src/parsers.js';
 import fs from "fs";
 import path from "path";
 import calculateDiff from "../src/calculateDiff.js";
-import stylish from '../formatter/stylish.js';
-import plain from '../formatter/plain.js';
+import createFormat from '../formatter/index.js';
 
 const getParesedData = (file) => {
     const data = fs.readFileSync(path.resolve(process.cwd(), '__fixtures__', file), 'utf-8');
     const dataType = path.extname(file).substring(1);
     return parse(data, dataType);
 };
-
-const createFormat = (file, formatName) => {
-    switch (formatName) {
-      case 'stylish': {
-        return stylish(file);
-      }
-      case 'plain': {
-        return plain(file);
-      }
-      default:
-        throw new Error('Invalid format.');
-    }
-  };
 
 const getDiff = (file1, file2, format = 'stylish') => {
     const diff = calculateDiff(getParesedData(file1), getParesedData(file2));
